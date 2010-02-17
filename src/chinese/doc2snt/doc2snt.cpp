095480b12aeb97cb40de66541fce4e36b8324864
frcchang <frcchang@709220ea-b82d-4b87-bd3a-7e1d81a5e10b>
2010-02-17 20:48:48 +0000

    add doc2snt

---
+++ src/chinese/doc2snt/doc2snt.cpp
@@ -0,0 +1,72 @@
+/****************************************************************
+ *                                                              *
+ * doc2snt.cpp - the chinese document to sentence converter     *
+ *                                                              *
+ * This implementation uses beam search ageanda, see chart.cpp  *
+ *                                                              *
+ * Author: Yue Zhang                                            *
+ *                                                              *
+ * Computing Laboratory, Oxford. 2009.8                         *
+ *                                                              *
+ ****************************************************************/
+
+#include "doc2snt.h"
+
+using namespace chinese;
+
+/*---------------------------------------------------------------
+ *
+ * fillBuffer - fill buffer
+ *
+ *--------------------------------------------------------------*/
+
+bool CDoc2Snt::fillBuffer() {
+   string chr;
+   bool retval = false;
+   // keep on reading
+   while (!m_buffer.full() && m_reader->readRawCharacter(&chr)) {
+      retval = true;
+      m_buffer.pushBack(chr);
+      // reading is segmented by EOL
+      if (chr=="\n") break;
+   }
+   return retval;
+}
+
+/*---------------------------------------------------------------
+ *
+ * getSentence - read sentence
+ *
+ * Find a sentence that ends with a punctuation, \n, or 
+ * maximum size. \n is always returned as a single sent
+ *
+ *--------------------------------------------------------------*/
+
+bool CDoc2Snt::getSentence(CStringVector &retval) {
+   // the reading is segmented by \n; otherwise it will wait until stdin fills
+   if ( m_buffer.size() == 0 || m_buffer[m_buffer.size()-1] != "\n" ) {
+      if ( !fillBuffer() ) return false;
+   }
+   unsigned long index = 0;
+   while (index<m_buffer.size()) {
+      if ( index==m_buffer.size()-1 || 
+           isSentenceSeparator(m_buffer[index]) || 
+           m_buffer[index]=="\n" ) { //
+         // continued punctuations, eg ."    )"    '" etc
+         while (index < m_buffer.size()-1 &&
+                isPunctuation(m_buffer[index]) && 
+                (isPunctuation(m_buffer[index+1])||m_buffer[index+1]=="\n")    //&&
+                ) 
+            ++ index;
+         // copy buffer
+         for (unsigned long i=0; i<=index; ++i) {
+            retval.push_back(m_buffer[i]);
+         }
+         ++index;
+         break;
+      } 
+      ++index;
+   }
+   m_buffer.popFront(index);
+   return true;
+}
---
+++ src/chinese/doc2snt/doc2snt.h
@@ -0,0 +1,63 @@
+/****************************************************************
+ *                                                              *
+ * doc2snt.h - the document to sentence convertor.              *
+ *                                                              *
+ * Author: Yue Zhang                                            *
+ *                                                              *
+ * Computing Laboratory, Oxford. 2009.8                         *
+ *                                                              *
+ ****************************************************************/
+
+#ifndef CHINESE_DOC2SNT_H
+#define CHINESE_DOC2SNT_H 
+
+#include "include.h"
+
+/*===============================================================
+ *
+ * Global items
+ *
+ *==============================================================*/
+
+namespace chinese {
+
+/*===============================================================
+ *
+ * CDoc2Snt - the sentence boundary detecter for Chinese 
+ *
+ *==============================================================*/
+
+class CDoc2Snt {
+
+protected:
+
+   const unsigned long m_nMaxSentSize;
+   CBufferCycle<string> m_buffer;
+   CSentenceReader *m_reader;
+
+public:
+
+   CDoc2Snt(const string &sFile, unsigned long nMaxSentSize) : m_nMaxSentSize(nMaxSentSize), m_buffer(nMaxSentSize){
+      m_reader = new CSentenceReader(sFile);
+   };
+
+   virtual ~CDoc2Snt() { 
+      delete m_reader;
+   };
+
+protected:
+
+   bool fillBuffer();
+
+public:
+
+   // returns whether a sentence is read.
+   bool getSentence(CStringVector &retval);
+
+};
+
+} // namespace chinese
+
+#endif
+
+
