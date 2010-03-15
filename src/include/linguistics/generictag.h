928195cf1080dd916b9e3e15bbcd164e8fd7631c
frcchang <frcchang@709220ea-b82d-4b87-bd3a-7e1d81a5e10b>
2010-03-15 16:36:41 +0000

    remove bidirectional english tagger

--- src/include/linguistics/generictag.h
+++ src/include/linguistics/generictag.h
@@ -21,10 +21,10 @@
  *
  *==============================================================*/
 
-class CStringTokenizer : public CTokenizer<string> {
+class CTagTokenizer : public CTokenizer<string> {
    public: 
-      CStringTokenizer() : CTokenizer<string>(3/*reserve for NONE BEGIN END*/) {} 
-      virtual ~CStringTokenizer() {}
+      CTagTokenizer() : CTokenizer<string>(3/*reserve for NONE BEGIN END*/) {} 
+      virtual ~CTagTokenizer() {}
 };
 
 /*===============================================================
@@ -39,20 +39,20 @@ public:
    enum {SENTENCE_BEGIN = 1};
    enum {SENTENCE_END = 2};
    enum {FIRST = 3};
-   static unsigned long COUNT;
-   static unsigned long COUNT_BITS;
-   static unsigned long LAST;
+   static unsigned long COUNT = 3;
+   static unsigned long COUNT_BITS = 2;
+   static unsigned long LAST = 2;
 
 protected:
    unsigned long m_code;
 
 protected:
    // static method assigns tokenizer as global dictionary of words
-   CStringTokenizer &getTokenizer() const {static CStringTokenizer tokenizer; return tokenizer;}
+   CTagTokenizer &getTokenizer() const {static CTagTokenizer tokenizer; return tokenizer;}
 
 public:
    CWord() { clear(); }
-   CWord(const string &s) { m_code=getTokenizer().lookup(s); }
+   CWord(const string &s) { load(s); }
    CWord(const CTag &t) { m_code=t.m_code; }
    CWord(const unsigned &u) { assert(getTokenizer().count()>u); m_code=u; }
    virtual ~CWord() {}
@@ -66,7 +66,16 @@ public:
    bool operator <= (const CWord &w) const { return m_code <= w.m_code; }
    bool operator >= (const CWord &w) const { return m_code >= w.m_code; }
 
-   void load(const string &s) { m_code = getTokenizer().lookup(s); }
+   void load(const string &s) {
+      m_code=getTokenizer().lookup(s); 
+      if (m_code>LAST) { 
+         assert(m_code == LAST+1);
+         ++COUNT; 
+         if ((1<<COUNT_BITS)>COUNT) ++COUNT_BITS;
+         ++LAST;
+         TRACE("Tag updated: count="<<COUNT<<",countbits="<<COUNT_BITS<<",last="<<LAST<<".");
+      } 
+   }
    const string &str() const { return getTokenizer().key(m_code); }
 
    void clear() { m_code=NONE; }
