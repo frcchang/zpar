976955251377fa7367715299a526f77cb5af791c
frcchang <frcchang@709220ea-b82d-4b87-bd3a-7e1d81a5e10b>
2010-04-06 14:03:03 +0000

    new script

---
+++ scripts/lang/common/conll2dep.py
@@ -0,0 +1,18 @@
+import sys
+
+def getIdx(s):
+   return str(int(s)-1)
+
+if __name__ == "__main__":
+   if len(sys.argv) != 2:
+      print "conll2dep input_file > output_file"
+      sys.exit(1)#return
+   file = open(sys.argv[1])
+   for line in file:
+      line = line.strip()
+      if not line:
+         print; continue
+      line = line.split("\t")
+      print "\t".join([getIdx(line[0]), line[1], line[4], getIdx(line[6]), line[7]])
+   file.close()
+
