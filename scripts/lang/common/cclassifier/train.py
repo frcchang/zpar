ed3b9666e38a6c4cdcc67ec74fb464133910c5a1
frcchang <frcchang@709220ea-b82d-4b87-bd3a-7e1d81a5e10b>
2010-05-07 14:44:32 +0000

    more scripts

---
+++ scripts/lang/common/cclassifier/train.py
@@ -0,0 +1,33 @@
+import extractfeat
+from svm import *
+
+sys.path.append(os.path.join(os.path.dirname(__file__), "../../../"))
+
+
+import tools.integerization
+
+def train(data, path):
+   print "Loading features."
+   integerizer = tools.integerization.CIntegerization()
+   labels = []
+   samples = []
+   for classid, feature in extractFeat(data, integerizer):
+      labels.append(classid)
+      samples.append(feature)
+   print "Training SVM."
+   problem = svm_problem(labels, samples)
+   param = svm_parameter(kernel_type = POLY, degree=2, C=1, eps=1, shrinking=0)
+   model = svm_model(problem, param)
+   print "Saving model."
+   os.mkdir(path)
+   model.save(os.path.join(path, "scr"))  
+   integerizer.write(os.path.join(path, "int"))
+
+if __name__ == "__main__":
+   opts, args = getopt.getopt(sys.argv[1:], "", [])
+
+   if len(args) < 2:
+      print "usage: train.py input model"
+      sys.exit(1)
+
+   train(args[0], args[1])
