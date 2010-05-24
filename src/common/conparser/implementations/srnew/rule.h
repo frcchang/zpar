f636660376ad6c8b298a3b717eb0bf140fbcaa92
frcchang <frcchang@709220ea-b82d-4b87-bd3a-7e1d81a5e10b>
2010-05-24 14:09:29 +0000

    wip

--- src/common/conparser/implementations/srnew/rule.h
+++ src/common/conparser/implementations/srnew/rule.h
@@ -9,6 +9,8 @@
 
 #include "tuple2.h"
 
+const vector<CAction> g_emptyVector;
+
 namespace TARGET_LANGUAGE {
 
 namespace conparser {
@@ -16,8 +18,8 @@ namespace conparser {
 class CRule {
 
 protected:
-   CHashMap< CTuple3<CAction, CConstituent, CConstituent>, unsigned > *m_mapBinaryRules;
-   CHashMap< CTuple2<CAction, CConstituent>, unsigned > *m_mapUnaryRules;
+   CHashMap< CTuple2<CConstituent, CConstituent>, vector<CAction> > *m_mapBinaryRules;
+   CHashMap< CConstituent, vector<CAction> > *m_mapUnaryRules;
    const vector< vector<CConstituent> > *m_LexConstituents;
 
 public:
@@ -51,69 +53,96 @@ public:
              item.nodes[item.stack.back()].head_left()==false) {
          }
          else {
-            action.encodeShift();
-            actions.push_back(action);
+             getShiftRules(item, actions);
          }
       }
       // reduce bin
       if ( stack_size > 1 ) {
-         const bool prev_temp = stack_size>2 ? item.nodes[item.stack[stack_size-3]].temp:false;
-	 for (unsigned long constituent=CConstituent::FIRST; constituent<CConstituent::COUNT; ++constituent) {
-	    for (unsigned i=0; i<=1; ++i) {
-	       for (unsigned j=0; j<=1; ++j) {
-		  const bool &head_left = static_cast<bool>(i);
-		  const bool &temporary = static_cast<bool>(j);
-		  const CStateNode &right = item.nodes[item.stack.back()];
-		  const CStateNode  &left = item.nodes[item.stack[stack_size-2]];
-		  assert( item.stack.back() == item.context->s0 );
-		  assert( item.stack[stack_size-2] == item.context ->s1 );
-		  const CWord &head_wd = item.sent->at( (head_left?left:right).lexical_head );
-		  if ( ( !left.temp || !right.temp ) &&
-		       ( !(stack_size==2 && item.current_word==item.sent->size()) || !temporary ) &&
-		       ( !(stack_size==2) || (!temporary||head_left) ) &&
-		       ( !(prev_temp && item.current_word==item.sent->size()) || !temporary ) &&
-		       ( !(prev_temp) || (!temporary||head_left) ) &&
-		       ( !left.temp || (head_left&&constituent==left.constituent.code()) ) &&
-		       ( !right.temp || (!head_left&&constituent==right.constituent.code()) ) //&&
-      //                 ( !temporary || CConstituent::canBeTemporary(constituent) ) 
-		     ) {
-		     action.encodeReduce(constituent, false, head_left, temporary);
-                     if (binaryRuleAllow(action, left.constituent, right.constituent)) {
-                        actions.push_back(action);
-                     } // if rules allow
-		  }
-               } // for j
-            } // for i
-         } // for constituent
+         getBinaryRules(item, actions);
       }
       // reduce unary
       if ( stack_size && item.unary_reduce<UNARY_MOVES &&
            !item.nodes[item.stack.back()].temp ) {
-         for (unsigned long constituent=CConstituent::FIRST; constituent<CConstituent::COUNT; ++constituent){
-            const CStateNode &child = item.nodes[item.stack.back()];
-            assert(item.context->s0==item.stack.back());
-            if (constituent != child.constituent.code()) { 
-               action.encodeReduce(constituent, true, false, false);
-               if (unaryRuleAllow(action, child.constituent)) {
-                  actions.push_back(action);
-               }
-            }
-         } // for constituent
+         getUnaryRules(item, actions);
       }
    }
 
-public:
-   bool binaryRuleAllow(const CAction &action, const CConstituent &c1, const CConstituent &c2) {
-      if (m_mapBinaryRules==0) return true;
-      static CTuple3<CAction, CConstituent, CConstituent> tuple3;
-      tuple3.refer(&action, &c1, &c2);
-      return m_mapBinaryRules->find(tuple3, 0);
+protected:
+   void getShiftRules(const CStateItem &item, vector<CAction> &actions) {
+      // the rules onto lexical item constituents
+      if (m_LexConstituents) {
+         for (int i=0; i<m_LexConstituents->at(item.current_word).size(); ++i) {
+            action.encodeShift(m_LexConstituents->at(item.current_word).at(i));
+            actions.push_back(action);
+         }
+         return;
+      }
+      // the default rules
+      static CAction action;
+      action.encodeShift();
+      actions.push_back(action);
    }
-   bool unaryRuleAllow(const CAction &action, const CConstituent &c) {
-      if (m_mapUnaryRules==0) return true;
-      static CTuple2<CAction, CConstituent> tuple2;
-      tuple2.refer(&action, &c);
-      return m_mapUnaryRules->find(tuple2, 0);
+   void getBinaryRules(const CStateItem &item, vector<CAction> &actions) {
+      static CAction action;
+      const unsigned &stack_size = item.stack.size();
+      const CStateNode &right = item.nodes[item.stack.back()];
+      const CStateNode  &left = item.nodes[item.stack[stack_size-2]];
+      // specified rules
+      if (m_mapBinaryRules) {
+         static CTuple2<CConstituent, CConstituent> tuple2;
+         tuple2.refer(left.consitutuent, right.constituent);
+         const vector<CAction> &result = m_mapBinaryRules->find(tuple2, g_emptyVector);
+         actions.insert(actions.end(), result.begin(), result.end());
+         return;
+      }
+      // the normal method
+      const bool prev_temp = stack_size>2 ? item.nodes[item.stack[stack_size-3]].temp:false;
+      for (unsigned long constituent=CConstituent::FIRST; constituent<CConstituent::COUNT; ++constituent) {
+         for (unsigned i=0; i<=1; ++i) {
+            for (unsigned j=0; j<=1; ++j) {
+	       const bool &head_left = static_cast<bool>(i);
+               const bool &temporary = static_cast<bool>(j);
+               assert( item.stack.back() == item.context->s0 );
+               assert( item.stack[stack_size-2] == item.context ->s1 );
+               const CWord &head_wd = item.sent->at( (head_left?left:right).lexical_head );
+               if ( ( !left.temp || !right.temp ) &&
+                     ( !(stack_size==2 && item.current_word==item.sent->size()) || !temporary ) &&
+                     ( !(stack_size==2) || (!temporary||head_left) ) &&
+                     ( !(prev_temp && item.current_word==item.sent->size()) || !temporary ) &&
+                     ( !(prev_temp) || (!temporary||head_left) ) &&
+                     ( !left.temp || (head_left&&constituent==left.constituent.code()) ) &&
+                     ( !right.temp || (!head_left&&constituent==right.constituent.code()) ) //&&
+//                     ( !temporary || CConstituent::canBeTemporary(constituent) ) 
+                 ) {
+                     action.encodeReduce(constituent, false, head_left, temporary);
+                        actions.push_back(action);
+                     } // if rules allow
+                  }
+               } // for j
+            } // for i
+         } // for constituent
+   }
+
+   void getUnaryRules(const CStateItem &item, vector<CAction> a&actions) {
+      const CStateNode &child = item.nodes[item.stack.back()];
+      // input rule list
+      if (m_mapUnaryRules) {
+         const vector<CAction> &result = m_mapUnaryRules->find(child.constituent, g_emptyVector);
+         actions.insert(actions.end(), result.begin(), result.end());
+         return;
+      }
+      // the normal rules
+      static CAction action;
+      const unsigned &stack_size = item.stack.size();
+      for (unsigned long constituent=CConstituent::FIRST; constituent<CConstituent::COUNT; ++constituent){
+         assert(item.context->s0==item.stack.back());
+         if (constituent != child.constituent.code()) { 
+            action.encodeReduce(constituent, true, false, false);
+            if (unaryRuleAllow(action, child.constituent)) {
+               actions.push_back(action);
+            }
+         }
+      } // for constituent
    }
 
 public:
@@ -129,7 +158,7 @@ public:
       if (s=="Free binary rules")
          return;
       ASSERT(!m_mapBinaryRules, "Binary rules already loaded");
-      m_mapBinaryRules = new CHashMap< CTuple3<CAction, CConstituent, CConstituent>, unsigned >;
+      m_mapBinaryRules = new CHashMap< CTuple2<CConstituent, CConstituent>, vector<CAction> >;
       is >> (*m_mapBinaryRules);
       // unary rules
       getline(is, s);
@@ -137,7 +166,7 @@ public:
       if (s=="Free unary rules")
          return;
       ASSERT(!m_mapUnaryRules, "Unary rules already loaded");
-      m_mapUnaryRules = new CHashMap< CTuple2<CAction, CConstituent>, unsigned >;
+      m_mapUnaryRules = new CHashMap< CConstituent, vector<CAction> >;
       is >> (*m_mapUnaryRules);
    }
 
@@ -162,7 +191,7 @@ public:
       if (!is.is_open()) {
          THROW("Loading binary rules failed possibly becaues file not exists.");
       }
-      m_mapBinaryRules = new CHashMap< CTuple3<CAction, CConstituent, CConstituent>, unsigned >;
+      m_mapBinaryRules = new CHashMap< CTuple2<CConstituent, CConstituent>, vector<CAction> >;
       is >> (*m_mapBinaryRules);
    }
 
@@ -170,7 +199,7 @@ public:
       if (!is.is_open()) {
          THROW("Loading unary rules failed possibly becaues file not exists.");
       }
-      m_mapUnaryRules = new CHashMap< CTuple2<CAction, CConstituent>, unsigned >;
+      m_mapUnaryRules = new CHashMap< CConstituent, vector<CAction> >;
       is >> (*m_mapUnaryRules);
    }
 
