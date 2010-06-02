e280a94ac6306f8c216e7f8acd7b0ab15aad1fc5
frcchang <frcchang@709220ea-b82d-4b87-bd3a-7e1d81a5e10b>
2010-06-02 19:56:35 +0000

    scripts.

--- scripts/ccg/evaluate.zpar.sh
+++ scripts/ccg/evaluate.zpar.sh
@@ -1,12 +1,13 @@
 scripts=~/local/src/cpp1/scripts
 candc=~/local/app/candc
+corpus=~/local/corpus/ccg
 
-#python $scripts/lang/chinese/ctbtools/unbinarize.py -op $1 >$1.pipe
-#rm $i.ccgbank_deps
-#count = `wc -l $1.pipe`
-#for i in `seq 1 $count`
-#do
-#  cat $1.pipe | head -$i | tail -1 >$1.pipe.part
+rm $1.ccgbank_deps
+count=`wc -l $1.pipe|cut -d\  -f1`
+for i in `seq 1 $count`;
+do
+  cat $1.txt | head -$i | tail -1 >$1.txt.part
+  python $scripts/lang/chinese/ctbtools/unbinarize.py -op $1.txt.part >$1.pipe.part
   python $scripts/ccg/pipe.py split $1.pipe.part $1.cat.part $1.pipe.fragmented.part
   $candc/bin/generate -e $candc/src/data/ccg/cats $candc/src/data/ccg/cats/markedup $1.pipe.fragmented.part >$1.ccgbank_deps.part
   python $scripts/ccg/mergefragmenteddeps.py $1.ccgbank_deps.part $1.pipe.fragmented.part >$1.ccgbank_deps.formatted.part
@@ -14,7 +15,8 @@ candc=~/local/app/candc
   cat $1.cat.part >>$1.ccgbank_deps
   rm $1.cat.part
   rm $1.ccgbank_deps.part
-#  rm $1.pipe.part
-#  rm $1.pipe.fragmented.part
+  rm $1.pipe.part
+  rm $1.pipe.fragmented.part
   rm $1.ccgbank_deps.formatted.part
-#done
+done
+$candc/src/scripts/ccg/evaluate $2.stagged $2.ccgbank_deps $1.ccgbank_deps
