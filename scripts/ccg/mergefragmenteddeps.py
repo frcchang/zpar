06f934e44dc612360b8903554860fb1a8170fe09
frcchang <frcchang@709220ea-b82d-4b87-bd3a-7e1d81a5e10b>
2010-06-02 19:56:35 +0000

    scripts.

--- scripts/ccg/mergefragmenteddeps.py
+++ scripts/ccg/mergefragmenteddeps.py
@@ -2,33 +2,38 @@ import sys
 
 STARTMATCH = '###<START>'
 
-def FindStartIdx(file):
+def FindStartIdx(file, default):
    line = file.readline()
    while line:
       if line.startswith(STARTMATCH):
          return int(line[len(STARTMATCH):-1])
       line = file.readline()
-   return -1
+   return default
 
 if __name__ == '__main__':
    file = open(sys.argv[1])
    filepipe = open(sys.argv[2])
-   start = FindStartIdx(filepipe)
+   start = FindStartIdx(filepipe, -1)
    bInit = False
-   assert start != -1
+   bFragmented = True
+   if start == -1:
+      bFragmented = False
+      start = 0
    for line in file:
       line = line.strip()
       if not line:
          if not bInit: continue
-         start = FindStartIdx(filepipe)
-         #assert start != -1
+         if bFragmented: start = FindStartIdx(filepipe, -1)
       elif line.startswith('<c>') or line.startswith('#'):
          continue
       else:
+         assert start != -1
          if not bInit: bInit = True
          words = line.split()
-         for index in (0, -2):
+         for index in (0, 3):
             word = words[index].rsplit('_', 1)
+            if len(word) < 2:
+               print >>sys.stderr, line
             word[1] = str(int(word[1]) + start)
             words[index] = '_'.join(word)
          print ' '.join(words)
