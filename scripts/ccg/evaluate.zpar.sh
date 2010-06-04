6a73ab97aec984ad324d74608ab5c2d56155bc95
frcchang <frcchang@709220ea-b82d-4b87-bd3a-7e1d81a5e10b>
2010-06-04 15:31:27 +0000

    more work progress

--- scripts/ccg/evaluate.zpar.sh
+++ scripts/ccg/evaluate.zpar.sh
@@ -1,6 +1,15 @@
 scripts=~/local/src/cpp1/scripts
 candc=~/local/app/candc
 corpus=~/local/corpus/ccg
+scratch=./scratch
+
+if [ -e $scratch ]
+then
+  echo $scratch dir already exists; exiting now
+  exit
+fi
+
+mkdir $scratch
 
 rm $1.ccgbank_deps
 echo '#' >>$1.ccgbank_deps
@@ -9,23 +18,32 @@ echo '' >>$1.ccgbank_deps
 count=`wc -l $1.txt|cut -d\  -f1`
 for i in `seq 1 $count`;
 do
-  cat $1.txt | head -$i | tail -1 >$1.txt.part
-  if [ `wc -w $1.txt.part|cut -d\  -f1` == 0 ] 
+  echo sentence $i
+  cat $1.txt | head -$i | tail -1 >$scratch/$1.txt.part.$i
+  if [ `wc -w $scratch/$1.txt.part.$i|cut -d\  -f1` == 0 ] 
   then 
+    echo parser failed.
     echo ''>>$1.ccgbank_deps
   else
-
-    python $scripts/lang/chinese/ctbtools/unbinarize.py -op $1.txt.part >$1.pipe.part
-    python $scripts/ccg/pipe.py split $1.pipe.part $1.cat.part $1.pipe.fragmented.part
-    $candc/bin/generate -j $candc/src/data/ccg/cats $candc/src/data/ccg/cats/markedup $1.pipe.fragmented.part >$1.ccgbank_deps.part
-    python $scripts/ccg/mergefragmenteddeps.py $1.ccgbank_deps.part $1.pipe.fragmented.part >$1.ccgbank_deps.formatted.part
-    grep ^[^\#] $1.ccgbank_deps.formatted.part >>$1.ccgbank_deps
-    cat $1.cat.part >>$1.ccgbank_deps
-    rm $1.cat.part
-    rm $1.ccgbank_deps.part
-    rm $1.pipe.part
-    rm $1.pipe.fragmented.part
-    rm $1.ccgbank_deps.formatted.part
+    
+    python $scripts/lang/chinese/ctbtools/unbinarize.py -op $scratch/$1.txt.part.$i >$scratch/$1.pipe.part.$i
+    python $scripts/ccg/pipe.py split $scratch/$1.pipe.part.$i $scratch/$1.cat.part.$i $scratch/$1.pipe.fragmented.part.$i
+    $candc/bin/generate -j $candc/src/data/ccg/cats $candc/src/data/ccg/cats/markedup $scratch/$1.pipe.fragmented.part.$i >$scratch/$1.ccgbank_deps.part.$i
+    python $scripts/ccg/mergefragmenteddeps.py $scratch/$1.ccgbank_deps.part.$i $scratch/$1.pipe.fragmented.part.$i $scratch/$1.cat.part.$i >$scratch/$1.ccgbank_deps.formatted.part.$i
+    if [ `../../../../app/candc/bin/generate -T $candc/src/data/ccg/cats/ $candc/src/data/ccg/cats/markedup $scratch/devo.pipe.fragmented.part.$i | grep '__PARSE_FAILED__' | wc -l | cut -d\  -f1` == 0 ]
+#    if [ `wc -w $scratch/$1.ccgbank_deps.formatted.part.$i|cut -d\  -f1` == 0 ] 
+    then
+      grep ^[^\#] $scratch/$1.ccgbank_deps.formatted.part.$i >>$1.ccgbank_deps
+      cat $scratch/$1.cat.part.$i >>$1.ccgbank_deps
+    else 
+      echo bin/generator failed
+      echo '' >>$1.ccgbank_deps
+    fi
+#    rm $1.cat.part
+#    rm $1.ccgbank_deps.part
+#    rm $1.pipe.part
+#    rm $1.pipe.fragmented.part
+#    rm $1.ccgbank_deps.formatted.part
   fi
 done
 $candc/src/scripts/ccg/evaluate $2.stagged $2.ccgbank_deps $1.ccgbank_deps
--- scripts/ccg/mergefragmenteddeps.py
+++ scripts/ccg/mergefragmenteddeps.py
@@ -1,41 +1,75 @@
 import sys
 
 STARTMATCH = '###<START>'
+ENDMATCHES = '<END>'
 
-def FindStartIdx(file, default):
+def FindStartAndEndIndex(file, default):
    line = file.readline()
    while line:
       if line.startswith(STARTMATCH):
-         return int(line[len(STARTMATCH):-1])
+         start, end = line[:-1].split()
+         assert end.startswith(ENDMATCHES)
+         return int(start[len(STARTMATCH):]), int(end[len(ENDMATCHES):])
       line = file.readline()
-   return default
+   return default, default
 
 if __name__ == '__main__':
    file = open(sys.argv[1])
    filepipe = open(sys.argv[2])
-   start = FindStartIdx(filepipe, -1)
-   bInit = False
+   filecats = open(sys.argv[3])
+   cat_line = filecats.readline()
+   cats = [word.split('|')[0] for word in cat_line.split()[1:]]
+   filecats.close()
+   nEmptyLineCount = 0
+   start, end = FindStartAndEndIndex(filepipe, -1)
+   nInit = 0 # uninitialized
    bFragmented = True
    if start == -1:
       bFragmented = False
       start = 0
+   bNothingFromBinGenerator = False
+   if start == end:
+      nEmptyLineCount -= 1
+   lines = []
    for line in file:
       line = line.strip()
       if not line:
-         if not bInit: continue
-         if bFragmented: start = FindStartIdx(filepipe, -1)
+         if nInit == 1: # empty line after initial comments
+            nInit = 2 # skip it
+            continue
+         if nInit == 0: # empty line before any comment
+            nInit = 2 # started
+         if nEmptyLineCount > 0:
+            bNothingFromBinGenerator = True
+            break
+         nEmptyLineCount += 1
+         if bFragmented: 
+            start, end = FindStartAndEndIndex(filepipe, -1)
+            if start==end:
+               nEmptyLineCount -= 1
       elif line.startswith('<c>') or line.startswith('#'):
+         assert not line.startswith('<c>')
+         if line.startswith('#') and nInit==0: nInit=1 # start with #
          continue
       else:
          assert start != -1
-         if not bInit: bInit = True
+         if nInit < 2: nInit = 2 # initialized
+         nEmptyLineCount = 0
          words = line.split()
          for index in (0, 3):
             word = words[index].rsplit('_', 1)
             if len(word) < 2:
                print >>sys.stderr, line
+            if not word[0] == cats[int(word[1]) + start - 1]:
+               print word[0], cats, int(word[1]), start-1
+            assert word[0] == cats[int(word[1]) + start - 1]
             word[1] = str(int(word[1]) + start)
             words[index] = '_'.join(word)
-         print ' '.join(words)
+         lines.append(' '.join(words))
+#   if bNothingBinGenerator: 
+#      print
+##   else: 
+   for line in lines: 
+      print line
    file.close()
    filepipe.close()
