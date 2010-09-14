c97ad194176b869d171ffee993a54306d5093912
frcchang <frcchang@709220ea-b82d-4b87-bd3a-7e1d81a5e10b>
2010-09-14 21:30:31 +0000

    tagger english

--- src/common/tagger/tagger_stateitem.h
+++ src/common/tagger/tagger_stateitem.h
@@ -12,33 +12,19 @@
 
 struct CStateItem {
    // words and tags
-   CTag *m_lTags;
-   unsigned long m_nLength;                      // the length of the sentence means the number of words it contains
+   const CStateItem *prev;
+   unsigned tag;
    // item score
    SCORE_TYPE m_nScore;
-   CStateItem() { m_lTags = new CTag[MAX_SENTENCE_SIZE]; clear(); }
-   ~CStateItem() { delete[] m_lTags; }
-   CStateItem(CStateItem& item) { cerr<<"CStateItem does not support copy constructor!"; cerr.flush(); assert(1==0); }
+   CStateItem() : prev(0), tag() { }
+   CStateItem(CStateItem& item) { THROW("CStateItem does not support copy constructor!"); }
    inline bool operator < (const CStateItem &item) const { return this->m_nScore < item.m_nScore; }
    inline bool operator > (const CStateItem &item) const { return this->m_nScore > item.m_nScore; }
-   void copy(const CStateItem *from) {
-      m_nLength = from->m_nLength;
-      m_nScore = from->m_nScore;
-      for (int i=0; i<m_nLength; ++i) {
-         m_lTags[i] = from->m_lTags[i];
-      }
-   }
-   void append(const unsigned long &tag) {
-      m_lTags[m_nLength] = tag;
-      ++m_nLength ;
-   }
-   void setTag(const unsigned long &index, const unsigned long &tag) {
-      m_lTags[index] = tag;
-   }
-   const CTag &getTag(const unsigned long &index) const {
-      return m_lTags[index];
-   }
-   inline void clear() { m_nScore = 0; m_nLength = 0; }
    inline SCORE_TYPE &score() { return m_nScore; }
+   inline void operator = (const CStateItem &item) {
+      prev = item.prev;
+      tag = item.tag;
+      m_nScore = item.m_nScore;
+   }
 };
 #endif
