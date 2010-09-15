339aea407397a86a571a99cc2def62206b943a07
frcchang <frcchang@709220ea-b82d-4b87-bd3a-7e1d81a5e10b>
2010-09-15 15:18:16 +0000

    wip

--- src/test.cpp
+++ src/test.cpp
@@ -7,7 +7,7 @@
  * Computing Laboratory, Oxford. 2006.10                 
  *                                                      
  ****************************************************************/
-
+#define SIMPLE_HASH
 #include "definitions.h"
 #include "table2.h"
 
@@ -23,17 +23,20 @@ public:
 
 void testtable2() {
    CTable2<string, int, double> table2;
-   ifstream is("input.txt");
-//   is >> table2;
-   cout << table2.lookup("One", 1);
-   cout << table2.lookup("Two", 3);
-   cout << table2.lookup("None", 1);
-//   cout << table2;
+   ifstream is("./input.txt");
+   is >> table2;
+   cout << table2.lookup("One", 1) << endl;
+   cout << table2.lookup("Two", 3) << endl;
+   cout << table2.lookup("None", 1) << endl;
+   cout << table2;
    is.close();
 }
 
 int main(int argc, char**argv){
+   try {
    cout << B::C << B::D << endl;
    cout << ~0UL << endl;
+   testtable2();
+   }catch(const string &s) { cout << s << endl; }
 };
 
