7d7b93da7ca54a0ea41cc55b21b58e949acbe696
frcchang <frcchang@709220ea-b82d-4b87-bd3a-7e1d81a5e10b>
2010-11-03 16:09:51 +0000

    wip packedscoremap

---
+++ src/include/learning/perceptron/hashmap_score_packed.h
@@ -0,0 +1,237 @@
+/****************************************************************
+ *                                                              *
+ * hashmap_score_packed.h - the definitions for hash map with   *
+ *                          packed score v.                     *
+ *                                                              *
+ * Author: Yue Zhang                                            *
+ *                                                              *
+ ****************************************************************/
+
+#ifndef _HASHMAP_SCORE_PACKED_H
+#define _HASHMAP_SCORE_PACKED_H
+
+#include "hash.h"
+#include "score.h"
+
+/*===============================================================
+ *
+ * CPackedScore - packed score definition
+ *
+ *==============================================================*/
+
+template <typename SCORE_TYPE, unsigned PACKED_SIZE>
+class CPackedScore {
+protected:
+   CScore<SCORE_TYPE> scores[PACKED_SIZE];
+public:
+   const SCORE_TYPE score(const unsigned &index, const int &n) const {
+      return scores[index].score(n);
+   }
+   void updateCurrent(const unsigned &index, const SCORE_TYPE &added, const int &round) {
+      scores[index].updateCurrent(added, round);
+   }
+   void updateAverage(const int &round) {
+      for (unsigned index=0; index<PACKED_SIZE; ++index)
+         scores[index].updateAverage(round);
+   }
+   void reset() {
+      for (unsigned index=0; index<PACKED_SIZE; ++index)
+         scores[index].reset();
+   }
+   bool empty() const {
+      for (unsigned index=0; index<PACKED_SIZE; ++index)
+         if (!scores[index].empty()) return false;
+      return true;
+   }
+public:
+   CScore<SCORE_TYPE> & operator [](const unsigned &index) { return scores[index]; }
+};
+
+template <typename SCORE_TYPE, unsigned PACKED_SIZE>
+istream & operator >> (istream &is, CPackedScore<SCORE_TYPE, PACKED_SIZE> &score) {
+   assert(PACKED_SIZE>0);
+   string s;
+   is >> s;
+   ASSERT(s=="[", "hashmap_score_packed.h: not well formatted CPackedScore");
+   is >> score[0];
+   for (unsigned index=1; index<PACKED_SIZE; ++index) {
+      is >> s;
+      ASSERT(s==",", "hashmap_score_packed.h: not well formatted CPackedScore");
+      is >> score[index];
+   }
+   is >> s;
+   ASSERT(s=="]", "hashmap_score_packed.h: not well formatted CPackedScore");
+   return is;
+}
+
+template <typename SCORE_TYPE, unsigned PACKED_SIZE>
+ostream & operator << (ostream &os, CPackedScore<SCORE_TYPE, PACKED_SIZE> &score) {
+   assert(PACKED_SIZE>0);
+   os << " [ "
+   os << score[0];
+   for (unsigned index=1; index<PACKED_SIZE; ++index)
+      os << " , " << score[index];
+   os << " ] ";
+   return os;
+}
+
+/*===============================================================
+ *
+ * CPackedScoreType - packed score type definition
+ *
+ *==============================================================*/
+
+template <typename SCORE_TYPE, unsigned PACKED_SIZE>
+class CPackedScoreType {
+protected:
+   SCORE_TYPE scores[PACKED_SIZE];
+public:
+   void reset() {
+      for (unsigned index=0; index<PACKED_SIZE; ++index)
+         scores[index]=0;
+   }
+   bool empty() const {
+      for (unsigned index=0; index<PACKED_SIZE; ++index)
+         if (scores[index]!=0) return false;
+      return true;
+   }
+public:
+   SCORE_TYPE operator [](const unsigned &index) {
+      assert(index<PACKED_SIZE);
+      return scores[index];
+   }
+   void operator +=(const CPackedScoreType &i) {
+      for (unsigned index=0; index<PACKED_SIZE; ++index)
+         scores[index]+=i.scores[index];
+   }
+   void operator add(const CPackedScore &i, const int &which) {
+      for (unsigned index=0; index<PACKED_SIZE; ++index)
+         scores[index]+=i.score(index, which);
+   }
+};
+
+/*===============================================================
+ *
+ * CPackedScoreMap - map to packed score definition
+ *
+ *==============================================================*/
+
+template <typename K, typename SCORE_TYPE, unsigned PACKED_SIZE>
+class CPackedScoreMap : public CHashMap< K , CPackedScore<SCORE_TYPE, PACKED_SIZE> > {
+
+protected:
+   const CPackedScore<SCORE_TYPE, PACKED_SIZE> m_zero ;
+
+public:
+   const string name ;
+   bool initialized ;
+   unsigned count ;
+
+public:
+   CPackedScoreMap(string input_name, int TABLE_SIZE, bool bInitMap=true) : name(input_name) , initialized(bInitMap) , count(0) , m_zero() , CHashMap<K,CPackedScore<SCORE_TYPE, PACKED_SIZE> >(TABLE_SIZE, bInitMap) {
+      assert(m_zero.empty());
+   }
+
+public:
+   virtual inline void init() {
+      initialized = true;
+      CHashMap<K, CPackedScore<SCORE_TYPE, PACKED_SIZE> >::init();
+   }
+
+   virtual inline SCORE_TYPE getScore( const K &key , const unsigned &index , const int &which ) {
+      return this->find( key , m_zero ).score( index , which );
+   }
+
+   virtual inline void updateScore( const K &key , const unsigned &index , const SCORE_TYPE &amount , const int &round ) {
+      (*this)[ key ].updateCurrent( index , amount , round );
+   }
+
+   virtual inline void getOrUpdateScore( CPackedScoreType<SCORE_TYPE, PACKED_SIZE> &out , const K &key , const unsigned &index , const int &which , const SCORE_TYPE &amount=0 , const int &round=0 ) {
+      if ( amount == 0 ) {
+         out.add(this->find(key, m_zero), which) ;
+      }
+      else {
+         assert( round != 0 );
+         updateScore( key , index , amount , round ) ;
+         return 0;
+      }
+   }
+
+   void computeAverage(unsigned long int round) {
+      count = 0;
+      typename CHashMap< K, CPackedScore<SCORE_TYPE, PACKED_SIZE> >::iterator it = this->begin();
+      while (it != this->end()) {
+         it.second().updateAverage(round) ;
+         ++ it;
+         ++ count;
+      }
+   }
+
+   void clearScores() {
+      typename CHashMap< K, CPackedScore<SCORE_TYPE, PACKED_SIZE> >::iterator it = this->begin();
+      while (it != this->end()) {
+         it.second().reset() ;
+         ++ it;
+      }
+   }
+
+#ifdef DEBUG
+public:
+   void trace() {
+      cout << name << ": ";
+      CHashMap< K , CPackedScore<SCORE_TYPE, PACKED_SIZE> >::trace();
+   }
+#endif
+};
+
+//===============================================================
+
+template<typename K, typename SCORE_TYPE, unsigned PACKED_SIZE>
+inline
+istream & operator >> (istream &is, CPackedScoreMap<K, SCORE_TYPE, PACKED_SIZE> &score_map) {
+   if (!is) return is ;
+   string s ;
+   getline(is, s) ;
+   // match name
+   const unsigned &size = score_map.name.size();
+   if ( s.substr(0, size)!=score_map.name ) THROW("the expected score map " << score_map.name << " is not matched.");
+   if ( !score_map.initialized ) {
+      // match size
+      if ( s.size()>size ) {
+         unsigned table_size = 0;
+         istringstream buffer(s.substr(size));
+         buffer >> table_size;
+         if (table_size) {
+            unsigned hash_size = 1;
+            while (hash_size<table_size) hash_size <<= 1;
+            hash_size <<= 1;
+            score_map.resize(hash_size);
+         }
+      }
+      score_map.init();
+   }
+   is >> static_cast< CHashMap< K, CScore<SCORE_TYPE> > &>(score_map) ;
+   return is ;
+}
+
+template<typename K, typename SCORE_TYPE, unsigned PACKED_SIZE>
+inline
+ostream & operator << (ostream &os, CPackedScoreMap<K, SCORE_TYPE, PACKED_SIZE> &score_map) {
+   assert(os);
+   if (score_map.count)
+      os << score_map.name << ' ' << score_map.count << endl ;
+   else
+      os << score_map.name << endl ;
+
+   typename CHashMap< K, CScore<SCORE_TYPE> >::iterator it = score_map.begin() ;
+   while ( it != score_map.end() ) {
+      if ( it.second().score(CScore<SCORE_TYPE>::eNonAverage)!=0 ||
+           it.second().score(CScore<SCORE_TYPE>::eAverage)!=0 )
+         os << it.first() << "\t:\t" << it.second() << endl ;
+      ++ it;
+   }
+   os << endl ;
+   return os ;
+}
+
+#endif
