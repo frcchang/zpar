0c0543550a376ba93ec480482a8704ab2f24a8ff
frcchang <frcchang@709220ea-b82d-4b87-bd3a-7e1d81a5e10b>
2010-11-11 17:30:51 +0000

    conll

--- src/libs/linguistics/conll.cpp
+++ src/libs/linguistics/conll.cpp
@@ -5,6 +5,8 @@ void readCoNLLFeats(std::vector<CCoNLLFeats> &outout, const std::string &input) 
    outout.clear();
    std::string line;
    getline(iss, line, '|');
+   if (line=="_")
+      return;
    while (iss && !line.empty()) {
       outout.push_back(CCoNLLFeats(line));
       getline(iss, line, '|');
