d44573480f049e2b490e864fa6fe922dc34ea7cc
frcchang <frcchang@709220ea-b82d-4b87-bd3a-7e1d81a5e10b>
2010-11-30 14:42:25 +0000

    wip scripts

---
+++ scripts/var/tagdict/formatdict.py
@@ -0,0 +1,24 @@
+import sys
+import gzip
+
+def formatdict(dDict, threshold):
+   for word in dDict:
+      total = 0
+      for tag in dDict[word]:
+         total += dDict[word][tag]
+      if total >= threshold:
+         for tag in dDict[word]:
+            print word, tag
+
+if __name__ == '__main__':
+   if sys.argv[1].endswith('.gz'):
+      file = gzip.open(sys.argv[1])
+   else:
+      file = open(sys.argv[1])
+   threshold = 1
+   if len(sys.argv) ==3:
+      threshold = int(sys.argv[2])
+   s = file.read()
+   d = eval(s)
+   formatdict(d, threshold)
+   file.close()
