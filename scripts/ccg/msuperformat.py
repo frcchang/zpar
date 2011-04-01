8433ba01dd678a6fc981806463d61932090bff6a
frcchang <frcchang@709220ea-b82d-4b87-bd3a-7e1d81a5e10b>
2011-04-01 15:19:41 +0000

    bru

--- scripts/ccg/msuperformat.py
+++ scripts/ccg/msuperformat.py
@@ -1,7 +1,25 @@
 import sys
 
-if __name__ == "__main__":
-   file = open(sys.argv[1])
+def insertprobabilities(path):
+   file = open(path)
+   for line in file:
+      line = line.strip()
+      if line:
+         line = line.split()
+         output = []
+         output.append(line[0])
+         output.append(line[1])
+         output.append(str(len(line)-2))
+         for index in range(2, len(line)):
+            output.append(line[index])
+            output.append('0')
+         print ' '.join(output)
+      else:
+         print
+   file.close()
+
+def removeprobabilities(path):
+   file = open(path)
    for line in file:
       line = line.strip()
       if line:
@@ -14,3 +32,6 @@ if __name__ == "__main__":
          print " ".join(output)
       else:
          print
+
+if __name__ == "__main__":
+   insertprobabilities(sys.argv[1])
