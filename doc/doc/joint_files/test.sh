22b5329faf2a8fadfce12169fe1d9e69f47fe540
zhumuhua <zhumuhua@709220ea-b82d-4b87-bd3a-7e1d81a5e10b>
2013-03-12 16:24:25 +0000

    zhumuhua commit files done

---
+++ doc/doc/joint_files/test.sh
@@ -0,0 +1,12 @@
+zpar=./zpar/dist/chinese.postagger
+
+rm model
+
+for i in `seq 1 10`;
+do
+$zpar/train train.txt model 1
+$zpar/tagger input.txt output.txt model 
+python evaluate.py output.txt reference.txt
+cp model model.$i
+gzip model.$i
+done
