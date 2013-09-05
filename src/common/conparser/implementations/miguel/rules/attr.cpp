f0c2d1b4882d353f4a999f68cb0a5a7badfcc7d9
migballesteros <migballesteros@709220ea-b82d-4b87-bd3a-7e1d81a5e10b>
2013-09-05 17:02:35 +0000

    attr3

--- src/common/conparser/implementations/miguel/rules/attr.cpp
+++ src/common/conparser/implementations/miguel/rules/attr.cpp
@@ -66,10 +66,96 @@
     	}
     	return false;
     }
-//"SBARQ < (WHNP|WHADJP=target $+ (SQ < (/^(?:VB|AUX)/ < " + copularWordRegex + " !$++ VP) !< (VP <- (PP <:IN)) !<- (PP <: IN)))
-bool buildAttr2() {
-// A<:B (B is the only child of A)
-}
+
+    //"SBARQ < (WHNP|WHADJP=target $+ (SQ < (/^(?:VB|AUX)/ < " + copularWordRegex + " !$++ VP) !< (VP <- (PP <:IN)) !<- (PP <: IN)))
+    bool buildAttr2() {
+    // A<:B (B is the only child of A)
+    //A <- B (B is the last child of A)
+    // A $+ B (A is the immediate left sister of B)
+    	if (node.constituent==PENN_CON_SBARQ){
+    		CStateNodeList* childsSbarq=node.m_umbinarizedSubNodes;
+    		while(childsSbarq!=0){
+    			const CStateNode* whTarg=childsSbarq->node;
+    			if ((whTarg->constituent==PENN_CON_WHNP || whTarg->constituent==PENN_CON_WHADJP) && (!isLinked(&node,whTarg))) {
+    				if (childsSbarq->previous!=0) {
+    					const CStateNode* sqSisterWh=childsSbarq->previous->node;
+    					if (sqSisterWh->constituent==PENN_CON_SQ) {
+    						bool secondCondition=true;
+    						bool thirdCondition=true;
+    						CStateNodeList* childsSq=sqSisterWh->m_umbinarizedSubNodes;
+    						while(childsSq!=0){
+    							const CStateNode* vpChildSq=childsSq->node;
+    							if (vpChildSq->constituent==PENN_CON_VP) {
+    								CStateNodeList* childsVp=vpChildSq->m_umbinarizedSubNodes;
+    								while(childsVp!=0){
+    									const CStateNode* ppChildVp=childsVp->node;
+    									if (childsVp->next==0 && ppChildVp->constituent==PENN_CON_PP) { //it is the last child of
+    										CStateNodeList* childsPP=ppChildVp->m_umbinarizedSubNodes;
+    										if (childsPP->next==0 && ((*words)[childsPP->node->lexical_head].tag.code()==PENN_TAG_IN)){
+    											secondCondition=false;
+    										}
+    									}
+    									childsVp=childsVp->next;
+    								}
+    							}
+    							childsSq=childsSq->next;
+    						}
+    						if (secondCondition) {
+    							childsSq=sqSisterWh->m_umbinarizedSubNodes;
+    							while(childsSq!=0) {
+    								const CStateNode* ppChildSq=childsSq->node;
+    								if (childsSq->next==0 && ppChildSq->constituent==PENN_CON_PP){
+    									CStateNodeList* childsPP=ppChildSq->m_umbinarizedSubNodes;
+    									if (childsPP->next==0 && ((*words)[childsPP->node->lexical_head].tag.code()==PENN_TAG_IN)){
+    										thirdCondition=false;
+    									}
+    								}
+    								childsSq=childsSq->next;
+    							}
+    						}
+    						if (secondCondition && thirdCondition){
+    							//(SQ < (/^(?:VB|AUX)/ < " + copularWordRegex + " !$++ VP)
+    							CStateNodeList* childsSq=sqSisterWh->m_umbinarizedSubNodes;
+    							while(childsSq!=0){
+    								const CStateNode* vbChildSq=childsSq->node;
+
+    								if ((*words)[vbChildSq->lexical_head].tag.code()==PENN_TAG_VERB) {
+    									const CStateNodeList* childsVb=vbChildSq->m_umbinarizedSubNodes;
+    									while(childsVb!=0){
+    										if ((compareWordToCopularWordRegex((*words)[childsVb->node->lexical_head].word))) {
+    											//A $++ B (A is a left sister of B)
+    											bool insideCondition=true;
+    											CStateNodeList* sistersVb=childsSq->next;
+    											while(sistersVb!=0){
+    												const CStateNode* vpSisterVb=sistersVb->node;
+    												if (vpSisterVb->constituent==PENN_CON_VP){
+    													insideCondition=false;
+    												}
+    												sistersVb=sistersVb->next;
+    											}
+    											if (insideCondition){
+    												CDependencyLabel* label=new CDependencyLabel(STANFORD_DEP_ATTR);
+    												if (buildStanfordLink(label, whTarg->lexical_head, node.lexical_head)) {
+    													addLinked(&node,whTarg);
+    												    return true;
+    												}
+    											}
+    										}
+    										childsVb=childsVb->next;
+    									}
+    								}
+    								childsSq=childsSq->next;
+    							}
+    						}
+    					}
+    				}
+    			}
+    			childsSbarq=childsSbarq->next;
+    		}
+    	}
+    	return false;
+    }
+
 //"SBARQ <, (WHNP|WHADJP=target !< WRB) <+(SQ|SINV|S|VP) (VP !< (S < (VP < TO)) < (/^(?:VB|AUX)/ < " + copularWordRegex + " $++ (VP < VBN|VBD)) !<- PRT !<- (PP <: IN) $-- (NP !< /^-NONE-$/))",
 bool buildAttr3() {
 
