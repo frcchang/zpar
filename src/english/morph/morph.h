a76a03618a5f0cad14dabca4ad2736890011c2ba
cgomezr <cgomezr@709220ea-b82d-4b87-bd3a-7e1d81a5e10b>
2013-09-20 10:13:32 +0000

    More English morphology code.

--- src/english/morph/morph.h
+++ src/english/morph/morph.h
@@ -20,7 +20,9 @@ namespace english
 
 enum MORPH_FIELDS {
 
-	//the numeric identifier of a field is also its offset in bits
+	//the numeric identifier of a field is also its offset in bits.
+	//these fields are also the feature types.
+	//for example a feature of type MORPH_VERB_TENSE with value MORPH_VERB_PRESENT can be extracted from the MORPH_VERB_TENSE bit field.
 
 	MORPH_POSTAG = 0,
 	MORPH_NOUN_TYPE = 6,
@@ -31,10 +33,12 @@ enum MORPH_FIELDS {
 	MORPH_VERB_TENSE = 17,
 	MORPH_PERSON = 19,
 	MORPH_DEGREE = 22,
-	MORPH_POSS = 24,
-	MORPH_WH = 26
+	MORPH_IS_POSS = 24,
+	MORPH_IS_WH = 26
 
 };
+
+const int LAST_MORPH_FIELD = MORPH_IS_WH;
 
 const int MORPH_FIELD_SIZE [] = {
 
@@ -54,21 +58,40 @@ const int MORPH_FIELD_SIZE [] = {
 	2, 0
 };
 
-enum MORPH_NOUN_TYPE {
+const std::string MORPH_FIELD_STRINGS [] = {
+
+	//MORPH_FIELD_SIZE[i] = name of the field that ends at bit i counting from the right hand side
+	//empty strings are as in MORPH_FIELD_SIZE
+
+	"POSTAG", "","","","",""
+	"NOUN TYPE", "",
+	"NUMBER", "",
+	"GENDER", "",
+	"VERB TYPE", "",
+	"VERB FORM", "","",
+	"VERB TENSE", "",
+	"PERSON", "","",
+	"DEGREE", "",
+	"IS POSS", "",
+	"IS WH", ""
+
+};
+
+enum MORPH_NOUN_TYPES {
 
 	MORPH_NOUN_TYPE_COMMON = 1,
 	MORPH_NOUN_TYPE_PROPER
 
 };
 
-enum MORPH_NUMBER {
+enum MORPH_NUMBERS {
 
 	MORPH_NUMBER_SINGULAR = 1,
 	MORPH_NUMBER_PLURAL
 
 };
 
-enum MORPH_GENDER {
+enum MORPH_GENDERS {
 
 	MORPH_GENDER_MASCULINE = 1,
 	MORPH_GENDER_FEMININE,
@@ -76,16 +99,169 @@ enum MORPH_GENDER {
 
 };
 
-//TODO continue
+enum MORPH_VERB_TYPES {
+
+	MORPH_VERB_MAIN = 1,
+	MORPH_VERB_MODAL
+
+};
+
+enum MORPH_VERB_FORMS {
+
+	MORPH_VERB_PERSONAL = 1, //indicative/subjunctive: VBP, VBZ, VBD
+	MORPH_VERB_INFINITIVE, //base form (VB)
+	MORPH_VERB_GERUND, //VBG
+	MORPH_VERB_PARTICIPLE //VBN
+
+};
+
+enum MORPH_VERB_TENSES {
+
+	MORPH_VERB_PRESENT = 1,
+	MORPH_VERB_PAST
+
+};
+
+enum MORPH_PERSONS {
+
+	MORPH_PERSON_FIRST = 1,
+	MORPH_PERSON_SECOND,
+	MORPH_PERSON_THIRD,
+	MORPH_PERSON_NOT_THIRD //VBP
+
+};
+
+enum MORPH_DEGREES {
+
+	MORPH_DEGREE_POSITIVE = 1,
+	MORPH_DEGREE_SUPERLATIVE,
+	MORPH_DEGREE_COMPARATIVE
+
+};
+
+enum MORPH_IS_POSS_VALUES { //unused at the moment. Give distinct values if we want to use it.
+
+	MORPH_NON_POSS = 0,
+	MORPH_POSS = 0
+
+};
+
+enum MORPH_IS_WH_VALUES { //unused at the moment. Give distinct values if we want to use it.
+
+	MORPH_NON_WH = 0,
+	MORPH_WH = 0
+
+};
+
+
+
+class CMorphFeat
+{
+
+	int featId;
+	unsigned long featValue;
+
+public:
+
+	CMorphFeat() : featId(0), featValue(0) {}
+	CMorphFeat( int featId , unsigned long featValue ) : featId(featId) , featValue(featValue) {}
+
+	virtual ~CMorphFeat() {}
+
+	unsigned long hash() const { return ((unsigned long)featId)*37 + featValue; }
+
+	void load(const int &id , const unsigned long & value) { featId = id; featValue = value; }
+
+public:
+
+   bool operator == (const CMorphFeat &f1) const { return (featId == f1.featId && featValue == f1.featValue); }
+   bool operator != (const CMorphFeat &f1) const { return (featId != f1.featId || featValue != f1.featValue); }
+   bool operator < (const CMorphFeat &f1) const { return ( featId < f1.featId ) || ( featId == f1.featId && featValue < f1.featValue ); }
+   bool operator > (const CMorphFeat &f1) const { return ( featId > f1.featId ) || ( featId == f1.featId && featValue > f1.featValue ); }
+   bool operator <= (const CMorphFeat &f1) const { return ( *this == f1 || *this < f1 ); }
+   bool operator >= (const CMorphFeat &f1) const { return ( *this == f1 || *this > f1 ); }
+
+   std::string str() const
+   {
+	   std::stringstream st;
+	   st << MORPH_FIELD_STRINGS[featId] << " = " << featValue;
+	   return st.str();
+   }
+
+};
+
 
 
 class CMorph
 {
 
-	long morphInfo;
+	unsigned long morphInfo;
+
+public:
+
+	CMorph() : morphInfo(0) {}
+	CMorph(unsigned long morphInfo) : morphInfo(morphInfo) { }
+
+	virtual ~CMorph() {}
+
+	unsigned long code() const { return morphInfo; }
+	unsigned long hash() const { return morphInfo; }
+	void copy(const CMorph &m) { morphInfo = m.morphInfo; }
+
+	void load(const unsigned long &i) { morphInfo = i; }
+
+public:
+
+   bool operator == (const CMorph &m1) const { return morphInfo == m1.morphInfo; }
+   bool operator != (const CMorph &m1) const { return morphInfo != m1.morphInfo; }
+   bool operator < (const CMorph &m1) const { return morphInfo < m1.morphInfo; }
+   bool operator > (const CMorph &m1) const { return morphInfo > m1.morphInfo; }
+   bool operator <= (const CMorph &m1) const { return morphInfo <= m1.morphInfo; }
+   bool operator >= (const CMorph &m1) const { return morphInfo >= m1.morphInfo; }
+
+
+public:
+
+   //get the value of a morphological information field
+   unsigned long getField ( MORPH_FIELDS field )
+   {
+	   assert(MORPH_FIELD_SIZE[field] > 0);
+	   unsigned long result = morphInfo >> field;
+	   unsigned long mask = ( 1ul << MORPH_FIELD_SIZE[field] - 1);
+	   return result & mask;
+   }
+
+   //clear a morphological information field to a blank (zero) value
+   void clearField ( MORPH_FIELDS field )
+   {
+	   assert(MORPH_FIELD_SIZE[field] > 0);
+	   unsigned long mask = ( 1ul << MORPH_FIELD_SIZE[field] - 1) << field; //1's only in that field
+	   morphInfo = morphInfo & (~mask); //clear the field to zeros
+   }
+
+   //set the value in a morphological information field
+   void setField ( MORPH_FIELDS field , unsigned long value )
+   {
+	   assert(MORPH_FIELD_SIZE[field] > 0);
+	   clearField(field);
+	   morphInfo = morphInfo | ( value << field );
+   }
+
+   std::vector<CMorphFeat> getFeatureVector()
+   {
+	   std::vector<CMorphFeat> result;
+	   for ( int field = MORPH_POSTAG ; field <= LAST_MORPH_FIELD ; field++ )
+	   {
+		   if ( MORPH_FIELD_SIZE[field] > 0 )
+		   {
+			   unsigned long val = getField ( (MORPH_FIELDS)field );
+			   if ( val != 0ul ) result.push_back( CMorphFeat(field,val) );
+		   }
+	   }
+	   return result;
+   }
 
 };
-
 
 
 
