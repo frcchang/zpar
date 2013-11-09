e79b5b0f638af6989745c3585c7b2808217dc9ec
migballesteros <migballesteros@709220ea-b82d-4b87-bd3a-7e1d81a5e10b>
2013-11-09 15:59:49 +0000

    quantmod

--- src/common/conparser/implementations/miguel/rules/quantmod.cpp
+++ src/common/conparser/implementations/miguel/rules/quantmod.cpp
@@ -1,5 +1,30 @@
 
 //"QP < IN|RB|RBR|RBS|PDT|DT|JJ|JJR|JJS|XS=target"
 bool quantmod1(){
-
+	if (node.constituent==PENN_CON_QP){
+		CStateNodeList* childsQp=node.m_umbinarizedSubNodes;
+		while(childsQp!=0){
+			const CStateNode* targ=childsQp->node;
+			if ((((*words)[targ->lexical_head].tag.code()==PENN_TAG_IN) || 
+				    			((*words)[targ->lexical_head].tag.code()==PENN_TAG_ADVERB)||
+				    			((*words)[targ->lexical_head].tag.code()==PENN_TAG_ADVERB_COMPARATIVE)||
+				    			((*words)[targ->lexical_head].tag.code()==PENN_TAG_ADVERB_SUPERLATIVE)||
+				    			((*words)[targ->lexical_head].tag.code()==PENN_TAG_PDT)||
+				    			((*words)[targ->lexical_head].tag.code()==PENN_TAG_DT)||
+				    			((*words)[targ->lexical_head].tag.code()==PENN_TAG_ADJECTIVE)||
+				    			((*words)[targ->lexical_head].tag.code()==PENN_TAG_ADJECTIVE_COMPARATIVE)||
+				    			((*words)[targ->lexical_head].tag.code()==PENN_TAG_ADJECTIVE_SUPERLATIVE)) && !(isLinked(&node,targ))) {
+				
+				
+				CDependencyLabel* label=new CDependencyLabel(STANFORD_DEP_QUANTMOD);
+				if (buildStanfordLink(label, targ->lexical_head, node.lexical_head)) {
+					addLinked(&node,targ);
+					return true;
+				}
+				    		
+			}
+			childsQp=childsQp->next;
+		}
+	}
+	return false;
 }
