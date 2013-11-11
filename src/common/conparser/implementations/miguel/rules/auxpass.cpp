df5976567cef7fabdce046235266fbb9b64ea7d2
migballesteros <migballesteros@709220ea-b82d-4b87-bd3a-7e1d81a5e10b>
2013-11-11 18:17:13 +0000

    auxpass1

--- src/common/conparser/implementations/miguel/rules/auxpass.cpp
+++ src/common/conparser/implementations/miguel/rules/auxpass.cpp
@@ -1,4 +1,74 @@
-//"VP < (/^(?:VB|AUX|POS)/=target < " + passiveAuxWordRegex + " ) < (VP|ADJP [ < VBN|VBD | < (VP|ADJP < VBN|VBD) < CC ] )",
+     //"VP < (/^(?:VB|AUX|POS)/=target < " + passiveAuxWordRegex + " ) < (VP|ADJP [ < VBN|VBD | < (VP|ADJP < VBN|VBD) < CC ] )",
+     bool auxpass1(){
+    	 if (node.constituent==PENN_CON_VP){
+    		 bool secondCondition=false;
+
+    		 CStateNodeList* childsVp=node.m_umbinarizedSubNodes;
+    		 while(childsVp!=0){
+    			 const CStateNode* vpadjp=childsVp->node;
+    			 if ((vpadjp->constituent==PENN_CON_VP)||(vpadjp->constituent==PENN_CON_ADJP)){
+    				 CStateNodeList* childsVpadjp=vpadjp->m_umbinarizedSubNodes;
+    				 while(childsVpadjp!=0){
+    					 if ((*words)[childsVpadjp->node->lexical_head].tag.code()==PENN_TAG_VERB_PAST ||
+                  				 (*words)[childsVpadjp->node->lexical_head].tag.code()==PENN_TAG_VERB_PAST_PARTICIPATE) {
+    						 secondCondition=true;
+    					 }
+    					 if (childsVpadjp->node->constituent==PENN_CON_VP || childsVpadjp->node->constituent==PENN_CON_ADJP){
+    						 CStateNodeList* childsInt=childsVpadjp->node->m_umbinarizedSubNodes;
+    						 while(childsInt!=0){
+    							 if ((*words)[childsInt->node->lexical_head].tag.code()==PENN_TAG_VERB_PAST ||
+    							     (*words)[childsInt->node->lexical_head].tag.code()==PENN_TAG_VERB_PAST_PARTICIPATE) {
+    								 CStateNodeList* childsVpadjp2=vpadjp->m_umbinarizedSubNodes;
+    								 while(childsVpadjp2!=0) {
+    									 if ((*words)[childsVpadjp2->node->lexical_head].tag.code()==PENN_TAG_CC) {
+    										 secondCondition=true;
+    									 }
+    									 childsVpadjp2=childsVpadjp2->next;
+    								 }
+    							 }
+    							 childsInt=childsInt->next;
+    						 }
+    					 }
+    					 childsVpadjp=childsVpadjp->next;
+    				 }
+    			 }
+    			 childsVp=childsVp->next;
+    		 }
+
+    		 if (secondCondition){
+    			 childsVp=node.m_umbinarizedSubNodes;
+    			 //VP < (/^(?:VB|AUX|POS)/=target < " + passiveAuxWordRegex + " )
+    			 while(childsVp!=0){
+    				 const CStateNode* verbTarg=childsVp->node;
+    				 //PENN_TAG_VERB, PENN_TAG_VERB_PAST, PENN_TAG_VERB_PROG, PENN_TAG_VERB_PAST_PARTICIPATE, PENN_TAG_VERB_PRES, PENN_TAG_VERB_THIRD_SINGLE,
+    				 if (((*words)[verbTarg->lexical_head].tag.code()==PENN_TAG_VERB ||
+    				     (*words)[verbTarg->lexical_head].tag.code()==PENN_TAG_VERB_PAST ||
+    				     (*words)[verbTarg->lexical_head].tag.code()==PENN_TAG_VERB_PROG ||
+    				     (*words)[verbTarg->lexical_head].tag.code()==PENN_TAG_VERB_PRES ||
+    				     (*words)[verbTarg->lexical_head].tag.code()==PENN_TAG_VERB_THIRD_SINGLE ||
+    				     (*words)[verbTarg->lexical_head].tag.code()==PENN_TAG_VERB_PAST_PARTICIPATE)&&!(isLinked(&node,verbTarg))) {
+
+    					 CStateNodeList* childsVerb=verbTarg->m_umbinarizedSubNodes;
+    					 while(childsVerb!=0){
+    						 if ((compareWordToPassiveAuxWordRegex((*words)[childsVerb->node->lexical_head].word))) {
+    							 CDependencyLabel* label=new CDependencyLabel(STANFORD_DEP_AUXPASS);
+    							 if (buildStanfordLink(label, verbTarg->lexical_head, node.lexical_head)) {
+    								 addLinked(&node,verbTarg);
+    							     return true;
+    							 }
+    						 }
+    						 childsVerb=childsVerb->next;
+    					 }
+
+    				 }
+    				 childsVp=childsVp->next;
+    			 }
+    		 }
+
+    	 }
+    	 return false;
+     }
+
 //bool auxpass1(){}
 
 //"SQ|SINV < (/^(?:VB|AUX|POS)/=target < " + beAuxiliaryRegex + " $++ (VP < VBD|VBN))",
