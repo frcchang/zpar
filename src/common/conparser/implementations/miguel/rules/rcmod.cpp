e48260260789094215f84d54b1fb99fd64ced420
migballesteros <migballesteros@709220ea-b82d-4b87-bd3a-7e1d81a5e10b>
2013-11-26 09:31:14 +0000

    rcmod4

--- src/common/conparser/implementations/miguel/rules/rcmod.cpp
+++ src/common/conparser/implementations/miguel/rules/rcmod.cpp
@@ -15,9 +15,40 @@ bool rcmod3(){
 }
 
 //"NP|WHNP|NML $++ RRC=target !$-- NP|WHNP|NML",
-bool rcmod4(){
+     bool rcmod4(){
+   	  CStateNodeList* childs=node.m_umbinarizedSubNodes;
+   	  while(childs!=0){
+   		  const CStateNode* head=childs->node;
+   		  if (head->constituent==PENN_CON_NP || head->constituent==PENN_CON_WHNP){
+   			  CStateNodeList* rightSisters=childs->next;
+   			  CStateNodeList* leftSisters=childs->previous;
+   			  bool leftCond=true;
+   			  while(leftSisters!=0){
+   				  if (leftSisters->node->constituent==PENN_CON_NP || leftSisters->node->constituent==PENN_CON_WHNP){
+   					  leftCond=false;
+   				  }
+   				  leftSisters=leftSisters->previous;
+   			  }
+   			  
+   			  if (leftCond){
+   				  while(rightSisters!=0){
+   					  const CStateNode* targ=rightSisters->node;
+   					  if (targ->constituent==PENN_CON_RRC && !isLinked(&node,targ)){
+   						  CDependencyLabel* label=new CDependencyLabel(STANFORD_DEP_ADVCL);
+   						  if (buildStanfordLink(label, targ->lexical_head, head->lexical_head)) {
+   							  addLinked(&node,targ);
+   						      return true;
+   						  }
+   					  }
+   					  rightSisters=rightSisters->next;
+   				  }
+   			  }
+   		  }
+   		  childs=childs->next;
+   	  }
+   	  return false;
+     }
 
-}
 
 //"@ADVP < (@ADVP < (RB < /where$/)) < @SBAR=target",
 bool rcmod5(){
