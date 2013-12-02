3aa49fdf9555b48691aebbeed6abe4914bdec855
migballesteros <migballesteros@709220ea-b82d-4b87-bd3a-7e1d81a5e10b>
2013-12-02 23:00:31 +0000

    num

--- src/common/conparser/implementations/miguel/rules/num.cpp
+++ src/common/conparser/implementations/miguel/rules/num.cpp
@@ -1,9 +1,53 @@
 //"/^(?:WH)?(?:NP|NX|NML)(?:-TMP|-ADV)?$/ < (CD|QP=target !$- CC)",
-bool num1(){
-
-}
+      bool num1(){
+     	  if (node.constituent==PENN_CON_WHNP || node.constituent==PENN_CON_NP || node.constituent==PENN_CON_NX){
+     		  CStateNodeList* childs=node.m_umbinarizedSubNodes;
+     		  while(childs!=0){
+     			  const CStateNode* targ=childs->node;
+     			  if ((targ->constituent==PENN_CON_QP || (*words)[targ->lexical_head].tag.code()==PENN_TAG_CD) && !isLinked(&node,targ)){
+     				  bool ccCond=true;
+     				  CStateNodeList* leftSisters=childs->previous;
+     				  if (leftSisters!=0){
+     					  if ((*words)[leftSisters->node->lexical_head].tag.code()==PENN_TAG_CC){
+     						  ccCond=true;
+     					  }
+     				  }
+     				  if (ccCond){
+     					  CDependencyLabel* label=new CDependencyLabel(STANFORD_DEP_NUM);
+     					  if (buildStanfordLink(label, targ->lexical_head, node.lexical_head)) {
+     						  addLinked(&node,targ);
+     					  	  return true;
+     					  }
+     				  }
+     			  }
+     			  childs=childs->next;
+     		  }
+     	  }
+     	  return false;
+       }
 
 //"/^(?:WH)?(?:NP|NX|NML)(?:-TMP|-ADV)?$/ < (ADJP=target <: QP)"
-bool num2(){
-
-}
+     bool num2(){
+   	  if (node.constituent==PENN_CON_WHNP || node.constituent==PENN_CON_NP || node.constituent==PENN_CON_NX){
+   		  CStateNodeList* childs=node.m_umbinarizedSubNodes;
+   		  while(childs!=0){
+   			  const CStateNode* targ=childs->node;
+   			  if (targ->constituent==PENN_CON_ADJP && !isLinked(&node,targ)){
+   				  bool qpCond=false;
+   				  CStateNodeList* childsT=targ->m_umbinarizedSubNodes;
+   				  if (childsT->node->constituent==PENN_CON_QP && childsT->next==0){
+   					  qpCond=true;
+   				  }
+   				  if (qpCond){
+   					  CDependencyLabel* label=new CDependencyLabel(STANFORD_DEP_NUM);
+   					  if (buildStanfordLink(label, targ->lexical_head, node.lexical_head)) {
+   						  addLinked(&node,targ);
+   					  	  return true;
+   					  }
+   				  }
+   			  }
+   			  childs=childs->next;
+   		  }
+   	  }
+   	  return false;
+     }
