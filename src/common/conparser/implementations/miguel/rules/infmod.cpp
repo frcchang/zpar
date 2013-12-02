ff1d0e361ed8231bcd863dd0343040dc6f740eed
migballesteros <migballesteros@709220ea-b82d-4b87-bd3a-7e1d81a5e10b>
2013-12-02 23:15:10 +0000

    infmod

--- src/common/conparser/implementations/miguel/rules/infmod.cpp
+++ src/common/conparser/implementations/miguel/rules/infmod.cpp
@@ -1,10 +1,97 @@
 
-//"/^NP(?:-[A-Z]+)?$/ < (S=target < (VP < TO) $-- NP|NN|NNP|NNS)",
-bool infmod1(){
+   //"/^NP(?:-[A-Z]+)?$/ < (S=target < (VP < TO) $-- NP|NN|NNP|NNS)",
+      bool infmod1(){
+    	  if (node.constituent==PENN_CON_NP){
+    		  CStateNodeList* childs=node.m_umbinarizedSubNodes;
+    		  while(childs!=0){
+    			  const CStateNode* targ=childs->node;
+    			  if (targ->constituent==PENN_CON_S && !isLinked(&node,targ)){
+    				  bool sistCond=false;
+    				  bool childsCond=false;
+    				  
+    				  CStateNodeList* leftSisters=childs->previous;
+    				  while(leftSisters!=0){
+    					  if (leftSisters->node->constituent==PENN_CON_NP || (*words)[leftSisters->node->lexical_head].tag.code()==PENN_TAG_NOUN
+    							  || (*words)[leftSisters->node->lexical_head].tag.code()==PENN_TAG_NOUN_PROPER || (*words)[leftSisters->node->lexical_head].tag.code()==PENN_TAG_NOUN_PLURAL){
+    						  sistCond=true;
+    					  }
+    					  leftSisters=leftSisters->previous;
+    				  }
+    				  CStateNodeList* childsT=targ->m_umbinarizedSubNodes;
+    				  while(childsT!=0){
+    					  if (childsT->node->constituent==PENN_CON_VP){
+    						  CStateNodeList* childsVp=childsT->node->m_umbinarizedSubNodes;
+    						  while(childsVp!=0){
+    							  if ((*words)[childsVp->node->lexical_head].tag.code()==PENN_TAG_TO){
+    								  sistCond=true;
+    							  }
+    							  childsVp=childsVp->next;
+    						  }
+    					  }
+    					  childsT=childsT->next;
+    				  }
+    				  if (sistCond && childsCond){
+    					  CDependencyLabel* label=new CDependencyLabel(STANFORD_DEP_INFMOD);
+    					  if (buildStanfordLink(label, targ->lexical_head, node.lexical_head)) {
+    						  addLinked(&node,targ);
+    					  	  return true;
+    					  }
+    				  }
+    			  }
+    			  childs=childs->next;
+    		  }
+    	  }
+    	  return false;
+      }
 
-}
+      //"/^NP(?:-[A-Z]+)?$/ < (SBAR=target < (S < (VP < TO)) $-- NP|NN|NNP|NNS)"
+            bool infmod2(){
+               	  if (node.constituent==PENN_CON_NP){
+               		  CStateNodeList* childs=node.m_umbinarizedSubNodes;
+               		  while(childs!=0){
+               			  const CStateNode* targ=childs->node;
+               			  if (targ->constituent==PENN_CON_SBAR && !isLinked(&node,targ)){
+               				  bool sistCond=false;
+               				  bool childsCond=false;
+               				  
+               				  CStateNodeList* leftSisters=childs->previous;
+               				  while(leftSisters!=0){
+               					  if (leftSisters->node->constituent==PENN_CON_NP || (*words)[leftSisters->node->lexical_head].tag.code()==PENN_TAG_NOUN
+               							  || (*words)[leftSisters->node->lexical_head].tag.code()==PENN_TAG_NOUN_PROPER || (*words)[leftSisters->node->lexical_head].tag.code()==PENN_TAG_NOUN_PLURAL){
+               						  sistCond=true;
+               					  }
+               					  leftSisters=leftSisters->previous;
+               				  }
+               				  CStateNodeList* childsT=targ->m_umbinarizedSubNodes;
+               				  while(childsT!=0){
+               					if (childsT->node->constituent==PENN_CON_S){
+               						CStateNodeList* childsS=childsT->node->m_umbinarizedSubNodes;
+               						while(childsS!=0){
+               							if (childsS->node->constituent==PENN_CON_VP){
+               								CStateNodeList* childsVp=childsS->node->m_umbinarizedSubNodes;
+               								while(childsVp!=0){
+               									if ((*words)[childsVp->node->lexical_head].tag.code()==PENN_TAG_TO){
+               										sistCond=true;
+               									}
+               									childsVp=childsVp->next;
+               								}
+               							}
+               							childsS=childsS->next;
+               						}
+               				  }
+               					  childsT=childsT->next;
+               				  }
+               				  if (sistCond && childsCond){
+               					  CDependencyLabel* label=new CDependencyLabel(STANFORD_DEP_INFMOD);
+               					  if (buildStanfordLink(label, targ->lexical_head, node.lexical_head)) {
+               						  addLinked(&node,targ);
+               					  	  return true;
+               					  }
+               				  }
+               			  }
+               			  childs=childs->next;
+               		  }
+               	  }
+               	  return false;
+                 }
 
-//"/^NP(?:-[A-Z]+)?$/ < (SBAR=target < (S < (VP < TO)) $-- NP|NN|NNP|NNS)"
-bool infmod2(){
-
-}
