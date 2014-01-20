3223624f6ec56c4db1431e8b9bc5aa0dc545a76e
migballesteros <migballesteros@709220ea-b82d-4b87-bd3a-7e1d81a5e10b>
2014-01-20 11:26:36 +0000

    mwe

--- src/common/conparser/implementations/miguel/rules/mwe.cpp
+++ src/common/conparser/implementations/miguel/rules/mwe.cpp
@@ -1,23 +1,108 @@
 
 //"PP|XS < (IN|TO < as|of|at|to|in) < (JJ|IN|JJR|JJS|NN=target < such|because|Because|least|instead|due|Due|addition|to)",
 inline const bool &mwe1(const unsigned long &cons) {
+    	if (cons==PENN_CON_PP){ //no xs
+    		bool firstCond=false;
+    		CStateNodeList* childs=node.m_umbinarizedSubNodes;
+    		bool firstCondition=false;
+    		while(childs!=0){
+    			if ((((*words)[childs->node->lexical_head].tag.code()==PENN_TAG_TO)
+    					|| ((*words)[childs->node->lexical_head].tag.code()==PENN_TAG_IN))
+    					&& ((*words)[childs->node->lexical_head].word==g_word_as || (*words)[childs->node->lexical_head].word==g_word_of || (*words)[childs->node->lexical_head].word==g_word_at
+    							|| (*words)[childs->node->lexical_head].word==g_word_to || (*words)[childs->node->lexical_head].word==g_word_in)) {
+    				firstCondition=true;
+    			}
+    			childs=childs->next;
+    		}
 
-}
+    		if (firstCondition){
+    			childs=node.m_umbinarizedSubNodes;
+    			while(childs!=0){
+    				if ((((*words)[childs->node->lexical_head].tag.code()==PENN_TAG_ADJECTIVE) || ((*words)[childs->node->lexical_head].tag.code()==PENN_TAG_NOUN)
+    						|| ((*words)[childs->node->lexical_head].tag.code()==PENN_TAG_ADJECTIVE_COMPARATIVE)
+    						|| ((*words)[childs->node->lexical_head].tag.code()==PENN_TAG_ADJECTIVE_SUPERLATIVE) || ((*words)[childs->node->lexical_head].tag.code()==PENN_TAG_IN))
+    						&& ((*words)[childs->node->lexical_head].word==g_word_such || (*words)[childs->node->lexical_head].word==g_word_because || (*words)[childs->node->lexical_head].word==g_word_Because ||
+    								(*words)[childs->node->lexical_head].word==g_word_least || (*words)[childs->node->lexical_head].word==g_word_instead || (*words)[childs->node->lexical_head].word==g_word_due ||
+    								(*words)[childs->node->lexical_head].word==g_word_Due || (*words)[childs->node->lexical_head].word==g_word_addition || (*words)[childs->node->lexical_head].word==g_word_to)
+    						&& !isLinked(&node,childs->node)) {
+    					if (buildStanfordLink(STANFORD_DEP_MWE, childs->node->lexical_head, node.lexical_head)) {
+    						addLinked(&node,childs->node);
+    					    return true;
+    					}
+    				}
+    				childs=childs->next;
+    			}
+    		}
+    	}
+    }
 
 //"ADVP < (RB|IN < well) < (IN|RB|JJS=target < as)",
-inline const bool &mwe2(const unsigned long &cons) {
+    inline const bool &mwe2(const unsigned long &cons) {
+    	if (cons==PENN_CON_ADVP){
+    		bool firstCond=false;
+    		CStateNodeList* childs=node.m_umbinarizedSubNodes;
+    		bool firstCondition=false;
+    		while(childs!=0){
+    			if ((((*words)[childs->node->lexical_head].tag.code()==PENN_TAG_ADVERB)
+    					|| ((*words)[childs->node->lexical_head].tag.code()==PENN_TAG_IN)) && ((*words)[childs->node->lexical_head].word==g_word_well)) {
+    				firstCondition=true;
+    			}
+    			childs=childs->next;
+    		}
 
-}
+    		if (firstCondition){
+    			childs=node.m_umbinarizedSubNodes;
+    			while(childs!=0){
+    				if ((((*words)[childs->node->lexical_head].tag.code()==PENN_TAG_ADVERB)
+    						|| ((*words)[childs->node->lexical_head].tag.code()==PENN_TAG_ADJECTIVE_SUPERLATIVE) || ((*words)[childs->node->lexical_head].tag.code()==PENN_TAG_IN))
+    						&& ((*words)[childs->node->lexical_head].word==g_word_as) && !isLinked(&node,childs->node)) {
+    					if (buildStanfordLink(STANFORD_DEP_MWE, childs->node->lexical_head, node.lexical_head)) {
+    						addLinked(&node,childs->node);
+    					    return true;
+    					}
+    				}
+    				childs=childs->next;
+    			}
+    		}
+    	}
+    }
 
 //"ADVP < (DT < all) < (CC < but)",
 inline const bool &mwe3(const unsigned long &cons) {
-
+	//no =target, thus no rule...
 }
 
 //"CONJP < (RB < rather|well|instead) < (RB|IN=target < as|than|of)",
 inline const bool &mwe4(const unsigned long &cons) {
+    	if (cons==PENN_CON_CONJP){
+    		bool firstCond=false;
+    		CStateNodeList* childs=node.m_umbinarizedSubNodes;
+    		bool firstCondition=false;
+    		while(childs!=0){
+    			if ((((*words)[childs->node->lexical_head].tag.code()==PENN_TAG_ADVERB))
+    					 && ((*words)[childs->node->lexical_head].word==g_word_well || (*words)[childs->node->lexical_head].word==g_word_rather || (*words)[childs->node->lexical_head].word==g_word_instead)) {
+    				firstCondition=true;
+    			}
+    			childs=childs->next;
+    		}
 
-}
+    		if (firstCondition){
+    			childs=node.m_umbinarizedSubNodes;
+    			while(childs!=0){
+    				if ((((*words)[childs->node->lexical_head].tag.code()==PENN_TAG_ADVERB)
+    						|| ((*words)[childs->node->lexical_head].tag.code()==PENN_TAG_IN))
+    						&& ((*words)[childs->node->lexical_head].word==g_word_as || (*words)[childs->node->lexical_head].word==g_word_than || (*words)[childs->node->lexical_head].word==g_word_of)
+    						&& !isLinked(&node,childs->node)) {
+    					if (buildStanfordLink(STANFORD_DEP_MWE, childs->node->lexical_head, node.lexical_head)) {
+    						addLinked(&node,childs->node);
+    					    return true;
+    					}
+    				}
+    				childs=childs->next;
+    			}
+    		}
+    	}
+    }
 
 //CONJP < (IN < in) < (NN|TO=target < addition|to)",
 inline const bool &mwe5(const unsigned long &cons) {
@@ -67,5 +152,6 @@ inline const bool &mwe5(const unsigned long &cons) {
 
 //"XS < JJR|JJS=target"
 inline const bool &mwe6(const unsigned long &cons) {
-
+	//what is xs?
+	//no xs, thus, no rule.
 }
