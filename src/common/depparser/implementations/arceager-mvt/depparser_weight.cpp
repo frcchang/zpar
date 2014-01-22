16ed2c0cda86c8027fb6a6a340cedf846b8f6e7e
frcchang <frcchang@709220ea-b82d-4b87-bd3a-7e1d81a5e10b>
2014-01-22 07:04:00 +0000

    multiview treebank parser

---
+++ src/common/depparser/implementations/arceager-mvt/depparser_weight.cpp
@@ -0,0 +1,107 @@
+// Copyright (C) University of Oxford 2010
+/****************************************************************
+ *                                                              *
+ * weight.cpp - the weight definition method                    *
+ *                                                              *
+ * Part of the dependency parser of english                     *
+ *                                                              *
+ * Yue Zhang                                                    *
+ * 2007.8                                                       *
+ *                                                              *
+ ****************************************************************/
+
+#include "depparser_weight.h"
+
+using namespace TARGET_LANGUAGE;
+using namespace TARGET_LANGUAGE::depparser;
+
+/*---------------------------------------------------------------
+ *
+ * loadScores - load scores from the file specified at constructor
+ *              currently this uses database, but it can be modified
+ *         
+ * Affects: m_bScoreModified, clearing it. 
+ *
+ *--------------------------------------------------------------*/
+
+void TARGET_LANGUAGE::depparser::CWeight::loadScores() {
+   clock_t time_start = clock();
+   std::cout<<"Loading scores..."; std::cout.flush();
+   std::ifstream file ; 
+   std::string s;
+   file.open(m_sRecordPath.c_str()) ;
+
+   if (!file.is_open()) {
+      std::cout << " empty." << std::endl; return;
+   }
+
+#ifdef LABELED
+   getline(file, s);
+   ASSERT(s=="Dependency labels:", "Dependency labels not found in model file") ;
+   getline(file, s);
+   std::istringstream iss(s);
+   CDependencyLabel label;
+   while(iss >> label);
+   getline(file, s);
+   ASSERT(s=="", "No empty line after the dependency labels") ;
+#endif
+   iterate_templates(file >>,;);
+
+   getline(file, s);
+   if (s=="Rules=1") {
+      setRules(true);
+   }
+   else {
+      ASSERT(s=="Rules=0", "Rules flag not found from the model file");
+      setRules(false);
+   }
+
+   file.close() ;
+   std::cout << " done. (" << double(clock()-time_start)/CLOCKS_PER_SEC << "s)" << std::endl;
+}
+
+/*---------------------------------------------------------------
+ *
+ * saveScores - save scores back to database
+ *
+ * This method is called by the destructor is m_bScoreModified
+ * is true.
+   std::cout<<"done."<<std::endl;
+ *
+ *--------------------------------------------------------------*/
+
+void TARGET_LANGUAGE::depparser::CWeight::saveScores() {
+   std::cout<<"Saving scores..."; std::cout.flush();
+   std::ofstream file ;
+   file.open(m_sRecordPath.c_str()) ;
+
+#ifdef LABELED
+   file << "Dependency labels:" << std::endl;
+   for (unsigned label=CDependencyLabel::FIRST; label<CDependencyLabel::COUNT; ++label)
+      file << CDependencyLabel(label) << ' ';
+   file << std::endl << std::endl;
+#endif
+   iterate_templates(file<<,;)
+#ifdef DEBUG
+   iterate_templates(,.trace(););
+#endif
+   if (m_bRules) file << "Rules=1" << std::endl;
+   else file << "Rules=0" << std::endl;
+
+   file.close();
+   std::cout<<" done."<<std::endl;
+}
+
+/*--------------------------------------------------------------
+ *
+ * computeAverageFeatureWeights - compute average feature weights
+ *
+ *-------------------------------------------------------------*/
+
+void TARGET_LANGUAGE::depparser::CWeight::computeAverageFeatureWeights(int round) {
+   std::cout<<"Computing averaged (total) feature vector..."; std::cout.flush();
+   iterate_templates(,.computeAverage(round);) ;
+
+   std::cout<<"done."<<std::endl;
+}
+
