5eb7757d15bc844f5121f3347f46bdb064aaca07
Nitin Madnani <nmadnani@ets.org>
2015-07-14 15:18:02 -0400

    Get Spanish zpar to compile
    - Since there is no constituency parser for zpar, remove constituency parsing from `zpar.es.cpp`.

--- src/spanish/zpar.es.cpp
+++ src/spanish/zpar.es.cpp
@@ -8,7 +8,6 @@
 #include "definitions.h"
 #include "options.h"
 #include "tagger.h"
-#include "conparser.h"
 #include "depparser.h"
 #include "reader.h"
 #include "writer.h"
@@ -61,55 +60,6 @@ void tag(const std::string sInputFile, const std::string sOutputFile, const std:
    delete tagger;
 
    std::cerr << "Tagging has finished successfully. Total time taken is: " << double(clock()-time_start)/CLOCKS_PER_SEC << std::endl;
-}
-
-/*===============================================================
- *
- * parse
- *
- *==============================================================*/
-
-void parse(const std::string sInputFile, const std::string sOutputFile, const std::string sFeaturePath) {
-   std::cerr << "Parsing started" << std::endl;
-   int time_start = clock();
-   std::ostream *outs; if (sOutputFile=="") outs=&std::cout; else outs = new std::ofstream(sOutputFile.c_str());
-   std::string sTaggerFeatureFile = sFeaturePath + "/tagger";
-   std::string sParserFeatureFile = sFeaturePath + "/conparser";
-   if (!FileExists(sTaggerFeatureFile))
-      THROW("Tagger model does not exists. It should be put at model_path/tagger");
-   if (!FileExists(sParserFeatureFile))
-      THROW("Parser model does not exists. It should be put at model_path/conparser");
-   std::cerr << "[tagger] ";
-   CTagger tagger(sTaggerFeatureFile, false);
-   std::cerr << "[parser] ";
-   CConParser conparser(sParserFeatureFile, false);
-   CSentenceReader input_reader(sInputFile);
-   CStringVector *input_sent = new CStringVector;
-   CTwoStringVector *tagged_sent = new CTwoStringVector;
-   english::CCFGTree *output_sent = new english::CCFGTree;
-
-   unsigned nCount=0;
-
-   CBitArray prunes(MAX_SENTENCE_SIZE);
-
-   // If we read segmented sentence, we will ignore spaces from input.
-   while( input_reader.readSegmentedSentenceAndTokenize(input_sent) ) {
-      TRACE("Sentence " << nCount);
-      ++ nCount;
-      if ( input_sent->back()=="\n" ) {
-         input_sent->pop_back();
-      }
-      tagger.tag(input_sent, tagged_sent, 1, NULL);
-      conparser.parse(*tagged_sent, output_sent);
-      // Ouptut sent
-      (*outs) << output_sent->str_unbinarized() << std::endl;
-   }
-   delete input_sent;
-   delete tagged_sent;
-   delete output_sent;
-
-   if (sOutputFile!="") delete outs;
-   std::cerr << "Parsing has finished successfully. Total time taken is: " << double(clock()-time_start)/CLOCKS_PER_SEC << std::endl;
 }
 
 /*===============================================================
@@ -176,7 +126,7 @@ int main(int argc, char* argv[]) {
    try {
       COptions options(argc, argv);
       CConfigurations configurations;
-      configurations.defineConfiguration("o", "{t|d|c}", "output format; 't' pos-tagged format in sentences, 'd' refers to dependency parse tree format, and 'c' refers to constituent parse tree format", "d");
+      configurations.defineConfiguration("o", "{t|d}", "output format; 't' pos-tagged format in sentences, and 'd' refers to dependency parse tree format", "d");
 
       if (options.args.size() < 2 || options.args.size() > 4) {
          std::cout << "\nUsage: " << argv[0] << " feature_path [input_file [output_file]]" << std::endl;
@@ -193,8 +143,6 @@ int main(int argc, char* argv[]) {
       std::string sOutFormat = configurations.getConfiguration("o");
       if (sOutFormat == "t")
          tag(sInputFile, sToFile, options.args[1]);//
-      else if (sOutFormat == "c" )
-         parse(sInputFile, sToFile, options.args[1]);//
       else if (sOutFormat == "d" )
          depparse(sInputFile, sToFile, options.args[1]);//
       return 0;
